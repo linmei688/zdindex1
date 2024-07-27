9205b8658eb20fb5e405b3ff042d1dc5df3ae760
linmei688 <166596388+linmei688@users.noreply.github.com>
2024-07-27 14:43:25 +0800

    创造1.js

---
+++ 1.js
@@ -0,0 +1,22 @@
+function isprime(n){
+    for(var i=2;i<=n/2;i++){
+    if(n%i===0){
+        break
+    
+         }
+            // console.log(i)
+      }
+
+ 
+
+// if(i<=n/2){
+//     // console.log("不是")
+//     return false
+// }else{
+//     // console.log("是")
+//     return true
+// }
+return i>n/2
+// 直接返回ture值
+
+}
